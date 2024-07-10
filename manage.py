1c5708c823be12774ab7d78218b8c779fa121b13
Satya Prakash <104289221+satyaprakashdhfm@users.noreply.github.com>
2024-07-10 13:04:26 +0530

    new

--- manage.py
+++ manage.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Utility for managing administrative tasks in Django."""
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
+    """Execute administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectmanagement.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError as err:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+            "Django couldn't be imported. Ensure it is installed and "
+            "available on your PYTHONPATH environment variable. Also, check if "
+            "the virtual environment is activated."
+        ) from err
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
